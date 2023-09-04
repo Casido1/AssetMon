b0a44985a17e66237debbf8b50eba2cb7a959d71
Casido1 <ugochukwu.anunihu@gmail.com>
2023-09-04 09:29:32 +0100

    Moved request payload for the Post and Put endpoints in Ownership controller

--- AssetMon.Presentation/Controllers/OwnershipController.cs
+++ AssetMon.Presentation/Controllers/OwnershipController.cs
@@ -1,11 +1,12 @@
 ﻿using AssetMon.Services.Interface;
+using AssetMon.Shared.DTOs;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace AssetMon.Presentation.Controllers
 {
     //[Authorize(Roles = "Administrator")]
-    [Route("api/ownership/userId/vehicleId")]
+    [Route("api/ownership")]
     [ApiController]
     public class OwnershipController : ControllerBase
     {
@@ -17,22 +18,23 @@ namespace AssetMon.Presentation.Controllers
         }
 
         [HttpPost]
-        public async Task<IActionResult> CreateOwnership(string userId, string vehicleId)
+        [Authorize(Roles = "Administrator")]
+        public async Task<IActionResult> CreateOwnership([FromBody] OwnershipDTO ownershipDTO)
         {
-            await _serviceManager.OwnershipService.CreateOwnershipAsync(userId, vehicleId, trackChanges: false);
+            await _serviceManager.OwnershipService.CreateOwnershipAsync(ownershipDTO.UserId, ownershipDTO.VehicleId, trackChanges: false);
 
             return NoContent();
         }
 
         [HttpPut]
-        public async Task<IActionResult> UpdateOwnership(string userId, string vehicleId)
+        public async Task<IActionResult> UpdateOwnership([FromBody] OwnershipDTO ownershipDTO)
         {
-            await _serviceManager.OwnershipService.UpdateOwnershipAsync(userId, vehicleId, trackChanges: true);
+            await _serviceManager.OwnershipService.UpdateOwnershipAsync(ownershipDTO.UserId, ownershipDTO.VehicleId, trackChanges: true);
 
             return NoContent();
         }
 
-        [HttpDelete]
+        [HttpDelete("{userId}/{vehicleId}")]
         public async Task<IActionResult> DeleteOwnership(string userId, string vehicleId)
         {
             await _serviceManager.OwnershipService.DeleteOwnershipAsync(userId, vehicleId, trackChanges: false);
---
+++ AssetMon.Shared/DTOs/OwnershipDTO.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AssetMon.Shared.DTOs
+{
+    public class OwnershipDTO
+    {
+        public string UserId { get; set; }
+        public string VehicleId { get; set; }
+    }
+}
